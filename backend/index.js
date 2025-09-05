115c66182e5099ad3dca6f0176117478690aa54a
Assad Faizi <assad.faizi@maersk.com>
2025-09-05 15:51:58 +0100

    Basic Backend Service Setup

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import homeRoute from './routes/homeRoute.js';
+
+const app = express();
+
+app.use('/', homeRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
